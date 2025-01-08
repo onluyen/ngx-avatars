14fbea0d5e770b0375f28bdfc445e3d1e7a7ee60
alejandro <ale.luis@gmail.com>
2025-01-08 15:11:01 -0300

    upgrade to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { Observable } from 'rxjs';
 import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent implements OnInit {
   userName = 'Haithem Mosbahi';
